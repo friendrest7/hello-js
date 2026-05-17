bd6b8566dfe04ecb7044f8826bfb72f6523e6f02
Friend Rest <friendrest7@gmail.com>
2026-05-17 15:47:24 +0200

    Updated console output

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Friend Rest";
+const age = 20;
+const favoriteProgrammingLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteProgrammingLanguage);
